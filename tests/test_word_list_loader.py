7cb2dbbd99231f905198166bf70c2b0fe606fa2d
Phil Hanna <ph1204@gmail.com>
2022-06-06 12:14:21 -0400

    Have load_word_list return a list, not a set

--- tests/test_word_list_loader.py
+++ tests/test_word_list_loader.py
@@ -5,9 +5,9 @@ from wordle import load_word_list
 class Test(TestCase):
 
     def test_load_word_list_good(self):
-        ws = load_word_list()
-        self.assertIn('ATONE', ws)
+        wl = load_word_list()
+        self.assertIn('ATONE', wl)
 
     def test_load_word_list_bad(self):
-        ws = load_word_list()
-        self.assertNotIn('asdfi', ws)
+        wl = load_word_list()
+        self.assertNotIn('asdfi', wl)
--- wordle/word_list_loader.py
+++ wordle/word_list_loader.py
@@ -7,5 +7,4 @@ def load_word_list():
     bs = pkgutil.get_data('wordle.data', 'word_list.json')
     with io.BytesIO(bs) as fp:
         wl = json.load(fp)
-        ws = set(wl)
-    return ws
+    return wl
