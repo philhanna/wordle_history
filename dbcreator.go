eabf0a152c69bb6a6121628da819654b82233084
Phil Hanna <ph1204@gmail.com>
2023-11-22 18:14:02 -0500

    Added dbcreator.go

---
+++ dbcreator.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"os"
+	"path/filepath"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// CreateDatabase creates an sqlite3 database in the system temporary
+// directory (/tmp on Linux) from the specified HTML data.
+func CreateDatabase(body string) error {
+
+	// Extract history data
+	history := GetScrapes(body)
+
+	// Create the database
+	tempDir := os.TempDir()
+	filename := filepath.Join(tempDir, "wordle_history.db")
+	log.Printf("Creating %q for %d history records...\n", filename, len(history))
+
+	// Delete any old version
+	os.Remove(filename)
+
+	// Create the new database
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	// Create the wordle history table
+	sql_create := `CREATE TABLE history (
+		date	TEXT,
+		puzzle	TEXT,
+		word	TEXT
+	);`
+	_, err = db.Exec(sql_create)
+	if err != nil {
+		return err
+	}
+
+	// Insert all the records
+	sql_insert := `INSERT INTO history VALUES(?, ?, ?);`
+	stmt, err := db.Prepare(sql_insert)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+
+	for _, answer := range history {
+		_, err := stmt.Exec(answer.Date, answer.Index, answer.Answer)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Done
+	log.Printf("Done.\n")
+	return nil
+}
