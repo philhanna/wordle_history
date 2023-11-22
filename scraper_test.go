7273437c2677b814e3290b25ebc20e64575be408
Phil Hanna <ph1204@gmail.com>
2023-11-22 17:54:03 -0500

    Added unit test for GetScrapes

--- scraper_test.go
+++ scraper_test.go
@@ -1,6 +1,11 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestToYYYYMMDD(t *testing.T) {
 	type args struct {
@@ -18,6 +23,24 @@ func TestToYYYYMMDD(t *testing.T) {
 			if got := ToYYYYMMDD(tt.args.yymmdd); got != tt.want {
 				t.Errorf("ToYYYYMMDD() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestGetScrapes(t *testing.T) {
+	tests := []struct {
+		name     string
+		filename string
+		expected int
+	}{
+		{"from test data", "testdata/answers.html", 886},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			body, err := os.ReadFile(tt.filename)
+			assert.Nil(t, err)
+			scrapes := GetScrapes(string(body))
+			assert.Equal(t, tt.expected, len(scrapes))
 		})
 	}
 }
