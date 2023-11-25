222174eb37c73f175846ff0d510aafc09ea41e78
Phil Hanna <ph1204@gmail.com>
2023-11-24 23:04:53 -0500

    Added unit test for LoadWords

---
+++ analysis/analyzer_test.go
@@ -0,0 +1,33 @@
+package analysis
+
+import (
+	"fmt"
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLoadWords(t *testing.T) {
+	tests := []struct {
+		name          string
+		filename      string
+		wantCount     int
+		wantFirstWord string
+		wantLastWord  string
+	}{
+		{"Check count and first word", "testdata/history.db", 889, "CIGAR", "THROW"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			words, err := LoadWords(tt.filename)
+			assert.Nil(t, err)
+			assert.Equal(t, tt.wantCount, len(words))
+			assert.Equal(t, tt.wantFirstWord, words[0])
+			assert.Equal(t, tt.wantLastWord, words[len(words)-1])
+			for i := 0; i < 10; i++ {
+				fmt.Printf("%d: %s\n", i, words[i])
+			}
+		})
+	}
+}
