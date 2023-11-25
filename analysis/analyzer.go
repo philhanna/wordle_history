8fae15c5ca7836b49b2bdd6637555f69b5054a19
Phil Hanna <ph1204@gmail.com>
2023-11-24 22:57:00 -0500

    Added LoadWords function

---
+++ analysis/analyzer.go
@@ -0,0 +1,36 @@
+package analysis
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+// LoadWords get the list of words from the history database
+func LoadWords(filename string) ([]string, error) {
+
+	// Connect to the database
+	db, err := sql.Open("sqlite3", filename)
+	if err != nil {
+		return nil, err
+	}
+	defer db.Close()
+
+	// Issue a query for the words in the history table
+	rows, err := db.Query(`SELECT word FROM history`)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	// Read from the query result into a slice of strings
+	words := make([]string, 0)
+	for rows.Next() {
+		var word string
+		if err := rows.Scan(&word); err != nil {
+			return nil, err
+		}
+		words = append(words, word) 
+	}
+
+	return words, nil
+}
