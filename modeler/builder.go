99c4c32e213a0cd5928b9e60988e72cb75c51a02
Phil Hanna <ph1204@gmail.com>
2023-11-25 21:19:17 -0500

    Renamed analysis package to modeler
    This will be the model builder

--- modeler/builder.go
+++ modeler/builder.go
@@ -1,4 +1,4 @@
-package analysis
+package modeler
 
 import (
 	"database/sql"
---
+++ modeler/doc.go
@@ -0,0 +1,5 @@
+// Package modeler creates a scoring model based on the characteristics
+// of the historical solution word list.
+package modeler
+
+const WORDLEN = 5
